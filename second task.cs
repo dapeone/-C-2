365d9983a45a9160c25e03663d224e4b637c1d85
dapeone <yaroslavtsiplenkov@gmail.com>
2025-03-01 10:40:50 +0700

    Update second task.cs

--- second task.cs
+++ second task.cs
@@ -1,33 +1,4 @@
-using System;
-
-class Program
-{
-    static int FirstTask(int baseNumber, int exponent)
-    {
-      Console.Write("Введите основание степени: ");
-      baseNumber = Convert.ToInt32(Console.ReadLine());
-      Console.Write("Введите показатель степени: ");
-      exponent = Convert.ToInt32(Console.ReadLine());
-
-      if (exponent == 0)
-      {
-        Console.WriteLine("Степень равна нулю, ответ: 1");
-        return 0;
-      }
-
-      int multiplier = baseNumber;
-
-      while (exponent > 1)
-      {
-        baseNumber *= multiplier;
-        --exponent;
-      }
-
-      Console.WriteLine(baseNumber);
-      return 0;
-    }
-
-    static int SecondTask(double valueOfX)
+      static int SecondTask(double valueOfX)
     {
       int numberOfCharacters = 0;
       double clone, secondDigit;
@@ -73,26 +44,3 @@ class Program
       Console.WriteLine(secondDigit);
       return 0;
     }
-
-    static void Main()
-    {
-      int taskNumber, exponent;
-      double baseNumber = 0, valueOfX = 0;
-
-      Console.Write("Введите номер задания: ");
-      taskNumber = Convert.ToInt32(Console.ReadLine());
-
-      if (taskNumber == 1)
-      {
-        int FirstTask((int)baseNumber, exponent);
-      }
-      else if (taskNumber == 2)
-      {
-        SecondTask(valueOfX);
-      }
-      else
-      {
-        Console.Error.WriteLine("Неверный номер задания. Введите 1 или 2");
-      }
-    }
-}
